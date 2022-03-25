53b556ec1feee1952421220b30776504cc43de9b
chench53 <chench53.logic@gmail.com>
2022-03-25 16:38:20 +0800

    add image list

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,31 @@
-import { useState, createContext } from 'react';
-
 import {Button} from 'react-bootstrap';
 import ThemeProvider from 'react-bootstrap/ThemeProvider'
 import Container from 'react-bootstrap/Container'
 
 import MyNfts from './my_nfts/my_nfts';
 import { Header } from './components/header';
+import ImgGallery from './views/img_gallery';
 // import ethApi from './eth_api/eth_api';
-import {
-  useEthers,
-  useCall,
-} from '@usedapp/core'
-import { Contract } from '@ethersproject/contracts'
+// import {
+//   useEthers,
+//   useCall,
+// } from '@usedapp/core'
+// import { Contract } from '@ethersproject/contracts'
 
 // import logo from './logo.svg';
 import './App.css';
 
-const UserContext = createContext(null);
 
 function App() {
   // const [account, setAccount] = useState('');
   // const ethereum = window.ethereum;
-  const { activateBrowserWallet, account, deactivate } = useEthers()
-  const contractAddress = '0xA129c36Fa5869862d934bf58d256bDBcBfB52A7f';
+  // const { activateBrowserWallet, account, deactivate } = useEthers()
+  // const contractAddress = '0xA129c36Fa5869862d934bf58d256bDBcBfB52A7f';
   // python web3.Web3.keccak(text='ownerOf(uint256)').substr(0, 8)
-  const methodSign = {
-    'ownerOf': '0x6352211e',
-    'balanceOf': '0x70a08231'
-  }
+  // const methodSign = {
+  //   'ownerOf': '0x6352211e',
+  //   'balanceOf': '0x70a08231'
+  // }
 
   var nfts = [
     {
@@ -77,6 +75,7 @@ function App() {
       <div className="App">
         <Container>
           <Header></Header>
+          <ImgGallery></ImgGallery>
           <MyNfts nfts={nfts}></MyNfts>
         </Container>
       </div>
---
+++ src/views/img_gallery.tsx
@@ -0,0 +1,49 @@
+import {Card, Button} from 'react-bootstrap';
+
+import './img_gallery.css';
+
+const ImgGallery = () => {
+  const imgs = {
+    meta: null,
+    list: [
+      {
+        name: 'lamp-a',
+        // file:  '/assets/lamp-a.jpg'
+      },
+      {
+        name: 'lamp-b',
+        // file:  '/assets/lamp-b.jpg'
+      },
+      {
+        name: 'lamp-c',
+        // file:  '/assets/lamp-c.jpg'
+      },
+    ]
+  }
+  const mint = (v: String) => {
+    console.log(v)
+  }
+
+  return (
+    <div>
+      <p>choose an image to mint a nft</p>
+      <div className="img-list">
+        {
+          imgs.list.map((img) => {
+            return (
+              <Card key={img.name} className='img-item'>
+                <Card.Title className='img-header'>
+                  {img.name}
+                </Card.Title>
+                <Card.Img src={process.env.PUBLIC_URL + `/assets/${img.name}.jpg`} className='img'></Card.Img>
+                <Button variant="primary" onClick={() => mint(img.name)}>mint</Button>
+              </Card >
+            )
+          })
+        }
+      </div>     
+    </div>
+  )
+}
+
+export default ImgGallery;
